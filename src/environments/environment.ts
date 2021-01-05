c052cea6bff56f5d5ac21bc422ad1aa567a21403
truongnh <truongnh.itvn@gmail.com>
2021-01-05 16:38:36 +0700

    edit mediaUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
   logServer: false,
   logClient: true,
   version: '1.0.0',
-  mediaUrl: 'http://222.252.25.241:10023/',
+  mediaUrl: 'http://222.252.25.241:10023/upload/',
   clientTimeout: 10000,
   idleTimeout: 1800
 };
