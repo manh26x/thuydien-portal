5ca9373aea9cc1b297e5048235310ec773a63b10
TruongNH <nhtruong.it@gmail.com>
2021-01-18 15:05:21 +0700

    add kpi module

---
+++ src/app/features/kpi/kpi-import/kpi-import.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aw-kpi-import',
+  templateUrl: './kpi-import.component.html',
+  styles: [
+  ]
+})
+export class KpiImportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
