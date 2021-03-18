19e66d485cae785d9794e1aafc835fcd4513d67a
TruongNH <nhtruong.it@gmail.com>
2021-03-18 10:16:15 +0700

    remove handle request timeout

--- src/app/core/request.interceptor.ts
+++ src/app/core/request.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import {AuthService} from '../auth/auth.service';
-import {timeout} from 'rxjs/operators';
+// import {timeout} from 'rxjs/operators';
 
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
@@ -12,12 +12,12 @@ export class RequestInterceptor implements HttpInterceptor {
   // url not required auth
   private readonly IGNORE_URLS = [];
   // url use import export timeout in environment, otherwise use client timeout
-  private readonly IMPORT_EXPORT_URLS = [
-    '/userPortal/exportUserExcel',
-    '/userPortal/readUserWithExcel',
-    '/portal/importKPI',
-    '/role/exportRoleExcel'
-  ];
+  //  private readonly IMPORT_EXPORT_URLS = [
+  //    '/userPortal/exportUserExcel',
+  //    '/userPortal/readUserWithExcel',
+  //    '/portal/importKPI',
+  //    '/role/exportRoleExcel'
+  //  ];
   constructor(private authService: AuthService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.url.includes(this.BASE_URL) && !this.IGNORE_URLS.find(u => req.url.includes(u))) {
@@ -25,10 +25,12 @@ export class RequestInterceptor implements HttpInterceptor {
         req = this.updateHeader(req);
       }
     }
-    const rqTimeout = this.IMPORT_EXPORT_URLS.find(u => req.url.includes(u)) ? environment.importExportTimeout : environment.clientTimeout;
-    return next.handle(req).pipe(
-      timeout(rqTimeout)
-    );
+    // const rqTimeout = this.IMPORT_EXPORT_URLS.find(u => req.url.includes(u)) ?
+    // environment.importExportTimeout : environment.clientTimeout;
+    return next.handle(req);
+    //    .pipe(
+    //      timeout(rqTimeout)
+    //    );
   }
 
   private updateHeader(req: HttpRequest<any>) {
