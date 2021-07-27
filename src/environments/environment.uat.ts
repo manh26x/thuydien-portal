d04589042a5ae872148764f5dd6f01dfda5371ad
AnhLND.PN <anhlnd.pn@tpb.com.vn>
2021-07-27 11:41:30 +0700

    - Add API config

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `configuration=docker`.
+
+/**
+ * baseUrl: Api Host
+ * basePath: Api Gateway context path
+ * mediaUrl: Folder files on server
+ * clientTimeout: client waiting api response time - Removed at commit 19e66d48
+ * importExportTimeout: client waiting api response time (Import, export feature) - Removed at commit 19e66d48
+ * logServer: Not Support now
+ * logClient: Log console
+ */
+export const environment = {
+  production: true,
+  tokenEx: 43199,
+  refreshTokenEx: 43199,
+  clientId: 'YnJvd3NlcjoxMjM0',
+  baseUrl: 'https://apisalesappsit.tpb.vn',
+  basePath: '',
+  logServer: false,
+  logClient: false,
+  version: '2.5.1',
+  mediaUrl: 'https://apisalesappsit.tpb.vn/upload/',
+  clientTimeout: 180000, // 3m
+  importExportTimeout: 300000, // 5m
+  idleTimeout: 1800
+};
