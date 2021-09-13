b33a69b9cd778eb62d9f6155241475c7740da407
Trần Hùng Mạnh <hungmanh2607@gmail.com>
2021-09-13 16:17:10 +0700

    _ comment add image, file

--- src/app/features/news/news-comment/news-comment.component.ts
+++ src/app/features/news/news-comment/news-comment.component.ts
@@ -2,17 +2,20 @@ import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core'
 import {BaseComponent} from '../../../core/base.component';
 import {CommentService} from '../service/comment.service';
 import {ActivatedRoute} from '@angular/router';
-import { finalize, map, takeUntil} from 'rxjs/operators';
+import {concatMap, finalize, map, takeUntil} from 'rxjs/operators';
 import {CommentRequest} from '../model/comment';
 import {TranslateService} from '@ngx-translate/core';
 import { TreeNode } from 'primeng/api';
 import {CommentEnum} from '../model/news.enum';
 import {Paginator} from 'primeng/paginator';
 import {IndicatorService} from '../../../shared/indicator/indicator.service';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, forkJoin, Observable, of} from 'rxjs';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {TreeTable} from 'primeng/treetable';
 import { saveAs } from 'file-saver';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {NewsService} from '../service/news.service';
+import {environment} from '../../../../environments/environment';
 @Component({
   selector: 'aw-news-comment',
   templateUrl: './news-comment.component.html',
@@ -25,10 +28,13 @@ export class NewsCommentComponent extends BaseComponent implements OnInit, After
   @ViewChild('contentIn') contentIn: any;
 
   @Input() isView: boolean;
-
+  isChangeImage = false;
+  filesImage: any[];
   commentTree: TreeNode[] = [];
   testEmitter$ = new BehaviorSubject<TreeNode[]>(this.commentTree);
 
+
+  baseUrl = '';
   pageSize = 10;
   totalItems: number;
   page = 0;
@@ -37,14 +43,22 @@ export class NewsCommentComponent extends BaseComponent implements OnInit, After
   commentEnum = CommentEnum;
   commentForm: FormGroup;
   contentValue = '';
+  previewUrl: SafeUrl;
+  isSelect = true;
+  fileName: any;
+  filesDoc = [];
+  isChangeDoc = true;
   constructor(
     private commentService: CommentService,
     private route: ActivatedRoute,
     private translate: TranslateService,
     private indicator: IndicatorService,
     private fb: FormBuilder,
+    private sanitizer: DomSanitizer,
+    private newsService: NewsService
   ) {
     super();
+    this.baseUrl = environment.baseUrl;
   }
 
   ngAfterViewInit(): void {
@@ -62,7 +76,9 @@ export class NewsCommentComponent extends BaseComponent implements OnInit, After
       idParent: [null],
       content: [''],
       idNews: [this.idNews],
-      type: [CommentEnum.CMT]
+      type: [CommentEnum.CMT],
+      imgPath: null,
+      filePath: null
     });
 
   }
@@ -81,6 +97,8 @@ export class NewsCommentComponent extends BaseComponent implements OnInit, After
 
 
   replyClicked(row, evt) {
+    this.clearImage();
+    this.doClearDoc();
     this.commentTree.forEach(cmt => {
       if (cmt.data.id === row.id) {
         cmt.expanded = true;
@@ -152,13 +170,38 @@ export class NewsCommentComponent extends BaseComponent implements OnInit, After
     if (body.content.trim() === '') {
       return;
     }
+    const listObs: Observable<string>[] = [];
+    if (this.filesImage.length > 0) {
+      const listFormData: FormData = new FormData();
+      listFormData.append('file', this.filesImage[0]);
+      listObs.push(this.newsService.uploadFile(listFormData));
+    } else {
+      listObs.push(of(null));
+    }
+
+    if (this.filesDoc.length > 0) {
+      const listFormData: FormData = new FormData();
+      listFormData.append('file', this.filesDoc[0]);
+      listObs.push(this.newsService.uploadFile(listFormData));
+    } else {
+      listObs.push(of(null));
+    }
     this.indicator.showActivityIndicator();
-    this.commentService.postComment(body).subscribe(res => {
-      this.contentValue = '';
-      this.commentForm.get('content').setValue('');
-      this.contentValue = '';
-      this.paging.changePage(this.page);
+    forkJoin(listObs).pipe(
+      concatMap(fileInfo => {
+        body.imgPath = fileInfo[0];
+        body.filePath = fileInfo[1];
+        return this.commentService.postComment(body);
+      })).subscribe(res => {
+        this.contentValue = '';
+        this.commentForm.get('content').setValue('');
+        this.contentValue = '';
+        this.filesImage = [];
+        this.clearImage();
+        this.doClearDoc();
+        this.paging.changePage(this.page);
     });
+
 
   }
 
@@ -173,12 +216,66 @@ export class NewsCommentComponent extends BaseComponent implements OnInit, After
     ).subscribe(res => {
       const myBlob: Blob = new Blob([res], { type: 'application/ms-excel' });
       saveAs(myBlob, 'comment_list.xlsx');
+    }, error => {
+      this.indicator.hideActivityIndicator();
+
+    });
+  }
+  doChangeImage(files) {
+    this.isChangeImage = true;
+    this.filesImage = files;
+    this.previewFile(files);
+    this.commentForm.patchValue({
+      image: files
     });
   }
 
+  onFileChange(event, type: string) {
+    if (event.target.files && event.target.files.length > 0) {
+      const files = event.target.files;
+      this.clearImage();
+      if (type === 'img') {
+        this.doChangeImage(files);
+      } else {
+        this.fileName = files[0].name;
+        this.isChangeDoc = false;
+        this.filesDoc = files;
+      }
+
+    }
+  }
+  doClearDoc() {
+    this.isChangeDoc = true;
+    this.filesDoc = [];
+    this.commentForm.patchValue({
+      filePath: ''
+    });
+  }
+
+  previewFile(files) {
+    const objUrl = window.URL.createObjectURL(files[0]);
+    this.previewUrl = this.sanitizer.bypassSecurityTrustUrl(objUrl);
+    this.isSelect = false;
+  }
+
+  doClearImage() {
+    this.isChangeImage = true;
+    this.filesImage = [];
+    this.commentForm.patchValue({
+      image: ''
+    });
+  }
   submit() {
     this.contentValue = '';
     this.onEnter();
   }
 
+  clearImage() {
+    this.isSelect = true;
+    this.doClearImage();
+  }
+
+  deleteClicked(rowData: any, $event: MouseEvent) {
+
+  }
 }
